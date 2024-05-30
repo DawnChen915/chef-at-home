fee25c087480747e25fbaf3df440811aca2fb4a1
陈琳 <317262095@qq.com>
2024-05-30 16:01:06 +0800

    仿奈雪的茶

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+
+import api from './api'
+import util from './common/util'
+import store from './store'
 import {
 	createSSRApp
 } from "vue";
 import App from "./App.vue";
 export function createApp() {
 	const app = createSSRApp(App);
+	app.use(store)
 	return {
 		app,
 	};
